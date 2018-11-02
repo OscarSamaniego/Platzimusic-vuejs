16527ad076070a5bbb06bae9ded1d4394543a9e1
OscarSamaniego <oscarsamaniego.m@gmail.com>
2018-11-01 20:36:15 -0500

    initial commit

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default{
+    appName : 'oscarmusic',
+    apiKey :'2e0d88be8f1866704831f91a2e5c9baa',
+    secret : 'bba65d88c3a34a09342438dbbcad191b' ,
+    registeredTo: 'osamaniegomelo'
+}
